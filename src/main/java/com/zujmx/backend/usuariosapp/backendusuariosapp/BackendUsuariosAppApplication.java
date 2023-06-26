4990efc5bae30a060ce39dccd2b281a088178b14
Jesus Zuñiga <jesuszt@hotmail.com>
2023-06-26 14:25:39 -0600

    First Commit

---
+++ src/main/java/com/zujmx/backend/usuariosapp/backendusuariosapp/BackendUsuariosAppApplication.java
@@ -0,0 +1,13 @@
+package com.zujmx.backend.usuariosapp.backendusuariosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUsuariosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUsuariosAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zujmx/backend/usuariosapp/backendusuariosapp/BackendUsuariosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zujmx.backend.usuariosapp.backendusuariosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUsuariosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
