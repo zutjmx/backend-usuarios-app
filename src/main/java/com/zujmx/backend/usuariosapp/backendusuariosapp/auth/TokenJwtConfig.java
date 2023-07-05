273b3388c10843609735d253151ea98d6f067c89
Jesus Zuñiga <jesuszt@hotmail.com>
2023-07-04 19:57:31 -0600

    Primeras configuraciones de JWT

--- src/main/java/com/zujmx/backend/usuariosapp/backendusuariosapp/auth/TokenJwtConfig.java
+++ src/main/java/com/zujmx/backend/usuariosapp/backendusuariosapp/auth/TokenJwtConfig.java
@@ -1,7 +1,14 @@
 package com.zujmx.backend.usuariosapp.backendusuariosapp.auth;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+
 public class TokenJwtConfig {
-    public final static String SECRET_KEY = "token_con_frase_secreta";
+    
+    //public final static String SECRET_KEY = "token_con_frase_secreta";
+    public final static Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     public final static String PREFIX_TOKEN = "Bearer ";
     public final static String HEADER_AUTHORIZATION = "Authorization";
+
 }
