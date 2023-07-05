b0f978313a97d3ffd682782aef13f34ee2f2d61e
Jesus Zuñiga <jesuszt@hotmail.com>
2023-07-05 16:18:40 -0600

    Se cambia a GenerationType.IDENTITY en Role

--- src/main/java/com/zujmx/backend/usuariosapp/backendusuariosapp/models/entities/Role.java
+++ src/main/java/com/zujmx/backend/usuariosapp/backendusuariosapp/models/entities/Role.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
 public class Role {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true)
