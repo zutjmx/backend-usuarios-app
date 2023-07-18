c7411a84ca4eae1ce28125e6655ac3120593dd86
Jesus Zuñiga <jesuszt@hotmail.com>
2023-07-18 14:19:13 -0600

    Se permiten todos los orígenes en el cors del SpringSecurityConfig

--- src/main/java/com/zujmx/backend/usuariosapp/backendusuariosapp/auth/SpringSecurityConfig.java
+++ src/main/java/com/zujmx/backend/usuariosapp/backendusuariosapp/auth/SpringSecurityConfig.java
@@ -60,7 +60,8 @@ public class SpringSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        //configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://localhost:8096/login","http://localhost:8097/login"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization","Content-Type"));
         configuration.setAllowCredentials(true);
